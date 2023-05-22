c632bc63bae9e7874a0b92c5667a7e3d9729f724
KevinBacklund <kebacklund@gmail.com>
2023-05-22 22:28:40 +0200

    requests

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+import requests
 #requests Get JSON
 #JSON -> Dict(år:befolkning)
 #Dict -> matplotlib graf
 
 def main():
+    req=requests.get("https://opendata.umea.se/api/records/1.0/search/?dataset=befolkningsfoeraendringar-helar&q=")
+    print(req.content)
 
 if __name__ == '__main__':
     main()
