a5684298cbf2a4e775e54f02d7a1d6da323235f3
KevinBacklund <kebacklund@gmail.com>
2023-05-23 21:29:07 +0200

    JSON undersökning påbörjad

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import requests
+import json
 #requests Get JSON
-#JSON -> Dict(år:befolkning)
+#JSON -> list[Dict(år:befolkning)]
 #Dict -> matplotlib graf
 
 def main():
     req=requests.get("https://opendata.umea.se/api/records/1.0/search/?dataset=befolkningsfoeraendringar-helar&q=")
-    print(req.content)
-
+    dict = json.loads(req.text)
+    with open("befolkning.json","w") as datafh:
+        json.dump(dict, datafh, indent=4)
+    with open("befolkning.json") as datafh:
+        data = json.load(datafh)
+    for key in data:
+        print(key) 
+    records = data["records"]
+    fields = []
+    for key in records:
+        fields.append(key["fields"])
+    print(fields)
 if __name__ == '__main__':
     main()
