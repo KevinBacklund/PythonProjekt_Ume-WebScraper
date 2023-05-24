6f8e62fdb2d90d4bf5d1df899e5b36464cebdf4a
KevinBacklund <kebacklund@gmail.com>
2023-05-24 21:02:35 +0200

    Refaktorering

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
 import requests
 import json
 import matplotlib.pyplot as plot
-#requests Get JSON
-#JSON -> Dict(år:befolkning
-#Dict -> matplotlib graf
+
 
 def main():
     req=requests.get("https://opendata.umea.se/api/records/1.0/search/?dataset=befolkningsfoeraendringar-helar&q=")
     dict = json.loads(req.text)
     with open("befolkning.json","w") as datafh:
         json.dump(dict, datafh, indent=4)
+    befolkning = joson_search()
+    befolkningsorted = {}
+    for ar in sorted(befolkning):
+        befolkningsorted.update({ar:befolkning[ar]})
+    graf(befolkningsorted)
+
+def graf(befolkningsorted):
+    fig, ax = plot.subplots()
+    ax.plot(befolkningsorted.keys(),befolkningsorted.values())
+    ax.set_title("Umeå befolkning")
+    ax.set_xlabel("år")
+    ax.set_ylabel("befolkning")
+    plot.show()
+
+def joson_search():
     with open("befolkning.json") as datafh:
         data = json.load(datafh)
     records = data["records"]
@@ -19,15 +32,7 @@ def main():
     befolkning = {}
     for field in fields:
         befolkning.update({field["ar"]:field["folkmangd"]})
-    print(befolkning)
-    befolkningsorted = {}
-    for a in sorted(befolkning):
-        befolkningsorted.update({a:befolkning[a]})
-    fig, ax = plot.subplots()
-    ax.plot(befolkningsorted.keys(),befolkningsorted.values())
-    ax.set_title("Umeå befolkning")
-    ax.set_xlabel("år")
-    ax.set_ylabel("befolkning")
-    plot.show()
+    return befolkning
+
 if __name__ == '__main__':
     main()
